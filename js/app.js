75c2d8b20f8909317fd10c0c41c99cee0370f7a3
Gerhard Lipp <gelipp@googlemail.com>
2012-07-04 14:06:54 +0200

    added JSON

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 define(['/js/ember.js.gz'], function(Ember) {
     Ember = window.Ember;
+    Ember.Handlebars.registerHelper("JSON",function(path) {
+        var value = Ember.getPath(this, path);
+        return JSON.stringify(value,null,2);
+    });
     window.JetViewer = Ember.Application.create({
         version: 'v0.1',
         status: 'offline'
