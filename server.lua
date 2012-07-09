5871703c02d732c02372798f413f0455603ed269
Gerhard Lipp <gelipp@googlemail.com>
2012-07-09 11:01:48 +0200

    code dup removed, redirect '/'

--- server.lua
+++ server.lua
@@ -1,34 +1,27 @@
 #!/usr/bin/env orbit
-local cjson = require'cjson'
+
 require'orbit'
 module("server", package.seeall, orbit.new)
 
-local serve_gziped_js = function(web)
-                           print('hallo',web.path_info)
-                           web.headers['Content-Type'] = 'text/javascript'
-                           web.headers['Content-Encoding'] = 'gzip'
-                           local file = io.open('./'..web.path_info)
-                           local data = file:read('*a')
-                           web.headers['Content-Length'] = #data
-                           print('asd')
-                           return data
-                        end
+local serve_gzipped = 
+   function(content_type)
+      return function(web)
+                web.headers['Content-Type'] = content_type
+                web.headers['Content-Encoding'] = 'gzip'
+                local file = io.open('./'..web.path_info)
+                local data = file:read('*a')
+                web.headers['Content-Length'] = #data
+                return data
+             end
+   end
 
-local serve_gziped_css = function(web)
-                           print('hallo',web.path_info)
-                           web.headers['Content-Type'] = 'text/css'
-                           web.headers['Content-Encoding'] = 'gzip'
-                           local file = io.open('./'..web.path_info)
-                           local data = file:read('*a')
-                           web.headers['Content-Length'] = #data
-                           print('asd')
-                           return data
-                        end
 
-print('hallo',cjson.encode({a=123}))
-server:dispatch_get(serve_gziped_js,"/.+%.js%.gz")
-server:dispatch_get(serve_gziped_css,"/.+%.css%.gz")
+server:dispatch_get(
+   function(web)      
+      web:redirect('/radar.html')
+   end,'/')
+server:dispatch_get(serve_gzipped('text/javascript'),"/.+%.js%.gz")
+server:dispatch_get(serve_gzipped('text/css'),"/.+%.css%.gz")
 server:dispatch_static("/.+")
---server:dispatch_static("/.+")
 
 return server
