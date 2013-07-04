bdef9090e23bfaca7578a2ec4dfd5344855cf6b7
Gerhard Lipp <gelipp@gmail.com>
2013-07-04 21:21:03 +0200

    move dispatch code to own function

--- radar.js
+++ radar.js
@@ -25,89 +25,91 @@ $(function() {
             to = to + 10;
         });
 
-        var changeFetch = function() {
-            var customMode = $('#fetch-custom-mode').prop('checked');
-            var fetchParams;
-            if (customMode) {
-                var fetchParamsString = $('#fetch-custom').val();
-                try {
-                    fetchParams = JSON.parse(fetchParamsString);
-                } catch (e) {
-                    alert('Sorry, invalid JSON:' + e);
-                }
-            } else {
-                fetchParams = {};
-                fetchParams.match = [$('#fetch-path').val()];
-                fetchParams.caseInsensitive = $('#fetch-path').prop('checked');
-                fetchParams.sort = {
-                    from: from,
-                    to: to
-                };
-            }
-            unfetch = jetInstance.fetch(fetchParams, function(n) {
-                    var label = $('#s' + n.index + ' .path');
-                    var value = $('#s' + n.index + ' .value');
-                    var button = $('#s' + n.index + ' button');
-                    if (n.event !== 'remove') {
-                        label.text(n.path);
-                        value.val(JSON.stringify(n.value));
-                        if (typeof n.value !== 'undefined' && n.value !== null) {
-                            button.text('set');
-                            button.off('click');
-                            button.on('click', function() {
-                                var val = $('#s' + n.index + ' .value').val();
-                                try {
-                                    val = JSON.parse(val);
-                                    jetInstance.set(n.path, val, function(err, result) {
-                                        if (err) {
-                                            alert('Set returned error: ' + JSON.stringify(err, null, 2));
-                                        }
-                                    });
-                                } catch (e) {
-                                    alert('Set failed: ' + e);
+        var dispatchFetch = function(n) {
+            var label = $('#s' + n.index + ' .path');
+            var value = $('#s' + n.index + ' .value');
+            var button = $('#s' + n.index + ' button');
+            if (n.event !== 'remove') {
+                label.text(n.path);
+                value.val(JSON.stringify(n.value));
+                if (typeof n.value !== 'undefined' && n.value !== null) {
+                    button.text('set');
+                    button.off('click');
+                    button.on('click', function() {
+                        var val = $('#s' + n.index + ' .value').val();
+                        try {
+                            val = JSON.parse(val);
+                            jetInstance.set(n.path, val, function(err, result) {
+                                if (err) {
+                                    alert('Set returned error: ' + JSON.stringify(err, null, 2));
                                 }
                             });
-                        } else {
-                            value.val('[]');
-                            button.text('call');
-                            button.off('click');
-                            button.on('click', (function() {
-                                    var args;
-                                    try {
-                                        args = JSON.parse(value.val());
-                                        if (!$.isArray(args)) {
-                                            throw (value.val() + "is no JSON Array");
-                                        }
-                                        jetInstance.call(n.path, args, function(err, result) {
-                                            if (err) {
-                                                alert('Call failed: ' + JSON.stringify(err, null, 2));
-                                            } else {
-                                                alert('Call returned: ' + JSON.stringify(result, null, 2));
-                                            }
-                                        });
-                                    } catch (e) {
-                                        alert('Invalid input: ' + e);
-                                    }
-                                });
-
-                            }
-                        } else {
-                            label.text('');
-                            value.val('');
-                        }
-                    },
-                    function(err) {
-                        if (err) {
-                            alert('fetching failed' + err);
-                        } else {
-                            var index;
-                            var i;
-                            for (i = from - 1; i < to; ++i) {
-                                index = i + 1;
-                                $('#content').append('<div id="s' + index + '">' + index + '<span class="path"></span><input type="text" class="value" editable></input><button></button></div>');
-                            }
+                        } catch (e) {
+                            alert('Set failed: ' + e);
                         }
                     });
+                } else {
+                    value.val('[]');
+                    button.text('call');
+                    button.off('click');
+                    button.on('click', (function() {
+                            var args;
+                            try {
+                                args = JSON.parse(value.val());
+                                if (!$.isArray(args)) {
+                                    throw (value.val() + "is no JSON Array");
+                                }
+                                jetInstance.call(n.path, args, function(err, result) {
+                                    if (err) {
+                                        alert('Call failed: ' + JSON.stringify(err, null, 2));
+                                    } else {
+                                        alert('Call returned: ' + JSON.stringify(result, null, 2));
+                                    }
+                                });
+                            } catch (e) {
+                                alert('Invalid input: ' + e);
+                            }
+                        });
+
+                    }
+                } else {
+                    label.text('');
+                    value.val('');
+                }
+
+            };
+
+            var changeFetch = function() {
+                var customMode = $('#fetch-custom-mode').prop('checked');
+                var fetchParams;
+                if (customMode) {
+                    var fetchParamsString = $('#fetch-custom').val();
+                    try {
+                        fetchParams = JSON.parse(fetchParamsString);
+                    } catch (e) {
+                        alert('Sorry, invalid JSON:' + e);
+                    }
+                } else {
+                    fetchParams = {};
+                    fetchParams.match = [$('#fetch-path').val()];
+                    fetchParams.caseInsensitive = $('#fetch-path').prop('checked');
+                    fetchParams.sort = {
+                        from: from,
+                        to: to
+                    };
+                }
+                unfetch = jetInstance.fetch(fetchParams, dispatchFetch, function(err) {
+                    if (err) {
+                        alert('fetching failed' + err);
+                    } else {
+                        var index;
+                        var i;
+                        for (i = from - 1; i < to; ++i) {
+                            index = i + 1;
+                            $('#content').append('<div id="s' + index + '">' + index + '<span class="path"></span><input type="text" class="value" editable></input><button></button></div>');
+                        }
+                    }
+                });
             };
 
             $('#fetch-config').submit(function(event) {
