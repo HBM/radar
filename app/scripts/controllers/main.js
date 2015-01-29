9dffd57ad08b78ef753fce0664bf8e1abf09d64e
Gerhard Preuss <gelipp@gmail.com>
2015-01-29 09:56:25 +0100

    connection management

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,44 +11,56 @@
 angular.module('radarApp')
   .controller('MainCtrl', ['$scope', '$jet', '$window', function ($scope, $jet, $window) {
     var storage = $window.localStorage;
-    storage['radar.connections'] = storage['radar.connections'] || '[{}]';
+    storage['radar.connections'] = storage['radar.connections'] || '[]';
     var storedConnections;
     try {
       storedConnections = JSON.parse(storage['radar.connections']);
     } catch(e) {
       storedConnections = [];
     }
-    var lastCon = storedConnections[storedConnections.length - 1];
-
-
-    $scope.url = lastCon && lastCon.url || '';
-    $scope.port = lastCon && lastCon.port || 80;
+    $scope.storedConnections = storedConnections;
+    var validateWsUrl = function(url) {
+      var regexp = /(ws|wss):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+      return regexp.test(url);
+    };
+    $scope.isValidWsUrl = false;
+    $scope.removeStoredConnection = function(con) {
+      storedConnections.splice(storedConnections.indexOf(con),1);
+    };
+    $scope.$watch('newUrl', function(url) {
+      $scope.isValidWsUrl = validateWsUrl(url);
+      //$scope.$apply();
+    });
+    $scope.storedConnections = storedConnections;
+    $scope.$watch('storedConnections', function(connections) {
+      console.log(connections);
+      storage['radar.connections'] = JSON.stringify(connections);
+    }, true);
+    $scope.newUrl = '';
     $scope.status = 'disconnected';
-    $scope.connect = function() {
+    $scope.disconnect = function() {
+      $scope.url = '';
+      $scope.peer.$close();
+      delete $scope.elements;
+      $scope.status = 'disconnected';
+    };
+    $scope.connect = function(url) {
       $scope.status = 'connecting';
       delete $scope.elements;
       if ($scope.peer) {
         $scope.peer.$close();
       }
       $scope.peer = new $jet.$Peer({
-        url: 'ws://' + $scope.url + ':' + $scope.port,
+        url: url,
         scope: $scope
       });
       $scope.peer.$connected.then(function() {
         $scope.status = 'connected';
-        var connection = {
-          port: $scope.port,
-          url: $scope.url
-        };
-        if (storedConnections.filter(function(stored) {
-          if (angular.equals(stored, connection)) {
-            return true;
-          }
-          return false;
-        }).length === 0) {
-          storedConnections.push(connection);
-          storage['radar.connections'] = JSON.stringify(storedConnections);
-        };
+        $scope.url = url;
+        if (storedConnections.indexOf(url) === -1) {
+          storedConnections.push(url);
+
+        }
       }, function() {
         $scope.status = 'disconnected';
       });
@@ -73,6 +85,7 @@ angular.module('radarApp')
         }
       });
     };
+  
 
     $scope.caseSensitive = false;
 
