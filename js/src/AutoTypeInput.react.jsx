654361d5bf3ef09fdec9f98d248121072cc9dd54
Gerhard Preuss <gelipp@gmail.com>
2015-09-09 11:07:37 +0200

    fix number input changing type to string

--- js/src/AutoTypeInput.react.jsx
+++ js/src/AutoTypeInput.react.jsx
@@ -1,9 +1,16 @@
 var React = require('react');
 var flatten = require('flat');
-var Response = require('./Response.react.jsx');
 var id = 0;
 
 class AutoTypeInput extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			invalid: false
+		};
+	}
+
 
 	render() {
 		var type = typeof this.props.value;
@@ -11,22 +18,59 @@ class AutoTypeInput extends React.Component {
 		var label = this.props.label;
 		var props = {};
 		props.id = id + label;
+		var errorMessage = '';
+		if (this.state.invalid) {
+			props.className = 'validate invalid';
+			errorMessage = this.state.errorMessage;
+			value = this.state.last;
+		}
 		++id;
-		props.onChange = this.props.onChange;
-		//		props.disabled = this.props.disabled;
+		props.onChange = (event) => {
+			if (type === 'number') {
+				if (event.target.value === '') {
+					this.setState({
+						invalid: true,
+						last: '',
+						errorMessage: 'number required'
+					});
+					return;
+				}
+				try {
+					this.setState({
+						invalid: false
+					});
+					event.target.typedValue = parseFloat(event.target.value);
+				} catch (e) {
+					this.setState({
+						invalid: true,
+						last: event.target.value,
+						errorMessage: 'not a number'
+					});
+					return;
+				}
+			} else if (type === 'boolean') {
+				event.target.typedValue = event.target.checked;
+			} else {
+				event.target.typedValue = event.target.value;
+			}
+			this.props.onChange(event);
+		};
+
+		props.disabled = this.props.readOnly;
 		props.readOnly = this.props.readOnly;
 		if (type === 'number') {
 			props.type = 'number';
 			props.value = value;
-			if (props.value.length === 0) {
-				props.className = 'valid invalid';
-			}
 			props.step = 'any';
 			props.required = true;
-
 		} else if (type === 'boolean') {
 			props.type = 'checkbox';
 			props.checked = value;
+		} else if (type === 'object') {
+			props.type = 'text';
+			props.value = '<empty object>';
+			props.disabled = true;
+			props.readOnly = true;
 		} else {
 			props.type = 'text';
 			props.value = value;
@@ -46,8 +90,8 @@ class AutoTypeInput extends React.Component {
 		} else {
 			return (
 				<div className="input-field col s12" key={label}>
-						<label htmlFor={props.id} className="active">{label}</label>
 						{React.DOM.input(props)}
+						<label data-error={errorMessage} htmlFor={props.id} className="active">{label}</label>
 					</div>
 			);
 		}
