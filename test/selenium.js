661c946aa2419cd8227b921bb7c8078d2d126f2b
Gerhard Preuss <gelipp@gmail.com>
2016-07-07 14:33:26 +0200

    add selenium basic test

---
+++ test/selenium.js
@@ -0,0 +1,40 @@
+/* global describe, before, after, it */
+
+const assert = require('assert')
+const webdriver = require('selenium-webdriver')
+const By = webdriver.By
+const express = require('express')
+const path = require('path')
+
+describe('Radar selenium', function () {
+  this.timeout(5000)
+  var driver
+  var server
+
+  before((done) => {
+    var app = express()
+    app.use('/', express.static(path.join(__dirname, '../')))
+    driver = new webdriver.Builder()
+      .forBrowser('firefox')
+      .build()
+    server = app.listen(8080, () => {
+      driver.get('http://localhost:8080')
+      done()
+    })
+  })
+
+  after(() => {
+    server.close()
+    driver.quit()
+  })
+
+  it('loads the app', (done) => {
+    driver
+      .findElement(By.css('#app'))
+      .then((element) => {
+        assert(element)
+        done()
+      })
+    .catch(done)
+  })
+})
