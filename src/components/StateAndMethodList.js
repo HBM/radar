253800cc9562534812992daa6255b390c1b75e84
Gerhard Preuss <gelipp@gmail.com>
2016-09-20 16:51:06 +0200

    add fields

--- src/components/StateAndMethodList.js
+++ src/components/StateAndMethodList.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { List, Row } from 'md-components'
+import { List, Row, Radiobutton, Select, Icon } from 'md-components'
+import flatten from 'flat'
 
 const methodAvatar = <span className='Method-avatar'>M</span>
 
@@ -17,13 +18,21 @@ const createMethodRow = (method, icon, link) => (
 
 const stateAvatar = <span className='State-avatar'>S</span>
 
-const createStateRow = (state, icon, link) => {
+const createStateRow = (state, icon, link, fields) => {
+  const flat = flatten(state.value)
+  fields = fields || Object.keys(flat).slice(0, 3)
+  const content = fields.map(field => {
+    return <span className='State-field'>
+      <span className='State-field-name'>{field}:</span>
+      <span className='State-field-value'>{JSON.stringify(flat[field])}</span>
+    </span>
+  })
   return (
     <Row
       onClick={() => {}} // iOS Safari does not get focus event if no click handler is installed
       avatar={stateAvatar}
       primary={state.path}
-      secondary={'State / ' + JSON.stringify(state.value)}
+      secondary={content}
       icon={icon}
       key={state.path}
       linkTo={link}
@@ -32,6 +41,15 @@ const createStateRow = (state, icon, link) => {
 }
 
 const StateAndMethodList = ({statesAndMethods, iconCreator = () => {}, rootPath}) => {
+  const fieldsObj = statesAndMethods
+    .map(state => Object.keys(flatten(state.value || {})))
+    .reduce((pre, fields) => {
+      fields.forEach(field => {
+        pre[field] = true
+      })
+      return pre
+    }, {})
+  const fields = Object.keys(fieldsObj)
   const rows = statesAndMethods
     .sort((a, b) => {
       if (a.path < b.path) {
@@ -48,14 +66,19 @@ const StateAndMethodList = ({statesAndMethods, iconCreator = () => {}, rootPath}
         return createMethodRow(method, iconCreator(method.path), rootPath + '/' + encodeURIComponent(method.path))
       } else {
         const state = stateOrMethod
-        return createStateRow(state, iconCreator(state.path), rootPath + '/' + encodeURIComponent(state.path))
+        return createStateRow(state, iconCreator(state.path), rootPath + '/' + encodeURIComponent(state.path), fields)
       }
     })
 
   return (
-    <List>
-      {rows}
-    </List>
+    <div>
+      <div style={{padding: '10px 24px'}}>
+        <Icon.Filter />
+      </div>
+      <List>
+        {rows}
+      </List>
+    </div>
   )
 }
 
